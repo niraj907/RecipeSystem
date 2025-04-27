b64d1862e13cae1c47e2e6b8a511da600afd0f34
Niraj Chaudhary <n45439687@gmail.com>
2025-04-27 11:48:50 -0700

    code has been updated useAuth and useLocalStorage and ProtedRoute

--- frontend/src/ProtectedRoute.jsx
+++ frontend/src/ProtectedRoute.jsx
@@ -8,3 +8,19 @@ const ProtectedRoute = () => {
 };
 
 export default ProtectedRoute;
+
+
+
+
+
+// import { Navigate } from "react-router-dom";
+// import { useAuth } from "../hooks/useAuth";
+
+// export const ProtectedRoute = ({ children }) => {
+//   const { user } = useAuth();
+//   if (!user) {
+//     // user is not authenticated
+//     return <Navigate to="/login" />;
+//   }
+//   return children;
+// };
